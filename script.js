766e82f6087e1e08bff208d9220d72c27fbf5f61
csDan31 <csDan31@gmail.com>
2022-10-23 23:59:26 -0400

    added consctructor/lib array/+book btn function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("test")
+// library array 
+let myLibrary = [];
+
+// book constructor
+function Book (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.info = function (){
+        return title + " by " + author + ", " + pages + " pages, " + read
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+console.log(theHobbit.info());
+
+// functions
+function addBookToLibrary(){
+
+}
+
+const addBookBtn = document.querySelector('.add-book');
+
+addBookBtn.addEventListener('click', () => {
+
+});
