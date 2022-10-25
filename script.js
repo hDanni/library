b5692a01650211488691c80f1344231c10441584
csDan31 <csDan31@gmail.com>
2022-10-25 00:03:08 -0400

    add functionality to add book btn to prompt form modal pop-up

--- script.js
+++ script.js
@@ -15,12 +15,32 @@ const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 console.log(theHobbit.info());
 
 // functions
+
 function addBookToLibrary(){
 
 }
 
-const addBookBtn = document.querySelector('.add-book');
+// modal pop-up functionality
+const addBookBtn = document.querySelector('.add-book-btn');
+const overlay = document.getElementById('overlay');
 
 addBookBtn.addEventListener('click', () => {
+    const modal = document.querySelector('.modal')
+    openModal(modal);
+})
 
-});
+overlay.addEventListener('click', () => {
+    const modals = document.querySelector('.modal.active')
+    closeModal(modals);
+})
+
+function openModal(modal){
+    if(modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+function closeModal(modal){
+    if(modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
